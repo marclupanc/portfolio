3acea4c27640fb407ac56e02903cd3e6a100661e
Marc <marclupanc@gmail.com>
2023-07-08 21:12:28 +0300

    Removing dummy data

--- src/components/Project.js
+++ src/components/Project.js
@@ -49,7 +49,7 @@ const Project = ({
             <em>{technologies}</em>
           </p>
           <h3 className="projectTitle">{title}</h3>
-          <span className="viewWork">View Work &#8594;</span>
+          <span className="viewWork">Vezi lucrarea &#8594;</span>
         </div>
         <div className="imageContainer col-6 d-flex align-items-center justify-content-center">
           <img src={image} alt="Laptop displaying the application" />
