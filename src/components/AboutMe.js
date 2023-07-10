ef8e6ab9e1c202ba0670c5856fc6dcbad0bf29f7
Marc <marclupanc@gmail.com>
2023-07-10 14:34:10 +0300

    new projects add

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -33,63 +33,88 @@ const AboutMe = ({ name, email, location, availability, brand }) => {
   return (
     // <p style={{ color: "grey" }}>Stay tuned for updates!</p>
     <div className="aboutContainer container">
-       <div className="row">
-         <motion.div
-           className="personalImage col-12 col-lg-4"
-           ref={ref}
-           initial={{ x: "-10vw", opacity: 0 }}
-           animate={inView ? { x: 0, opacity: 1 } : { x: "-10vw", opacity: 0 }}
-           transition={{ duration: 0.4, ease: "easeInOut" }}
+      <div className="row">
+        <motion.div
+          className="personalImage col-12 col-lg-4"
+          ref={ref}
+          initial={{ x: "-10vw", opacity: 0 }}
+          animate={inView ? { x: 0, opacity: 1 } : { x: "-10vw", opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
-           <img src={aboutMeImg} alt={name} />
-         </motion.div>
-         <motion.div
-           className="personalInfo col-12 col-lg-8"
-           ref={ref}
-           initial={{ x: "10vw", opacity: 0 }}
-           animate={inView ? { x: 0, opacity: 1 } : { x: "10vw", opacity: 0 }}
-           transition={{ duration: 0.4, ease: "easeInOut" }}
-         >
-           <div className="contentContainer">
-             <h4>Bine ai venit</h4>
-             <h5>Sunt Front End Developer</h5>
-             <div className="contentDescription">
-               <p>{brand}</p>
-             </div>
-             <div className="infoContainer">
-               <div className="row">
-                 <div className="col-12 col-md-6 info">
-                   <span>Nume:</span>
-                   <p>Marc Lupanciuc</p>
-                 </div>
-                 <div className="col-12 col-md-6 info">
-                   <span>Email:</span>
-                   <p>
-                     <a href={`mailto:${email}`}>{email}</a>
-                   </p>
-                 </div>
-               </div>
-               <div className="row">
-                 <div className="col-12 col-md-6 info">
-                   <span>Locația: </span>
-                   <p>{location}</p>
-                 </div>
-                 <div className="col-12 col-md-6 info">
-                   <span>Disponibilitatea:</span>
-                   <p>{availability}</p>
-                 </div>
-               </div>
-             </div>
-             <div className="buttonContainer">
-               <button className="btn downloadCV" onClick={handleDownload} disabled={downloading}>
-                 {downloading ? "Se descarcă..." : "Descarcă CV"}
-               </button>
-               <SocialIcons />
-             </div>
-           </div>
-         </motion.div>
-       </div>
-     </div>
+          <img src={aboutMeImg} alt={name} />
+        </motion.div>
+        <motion.div
+          className="personalInfo col-12 col-lg-8"
+          ref={ref}
+          initial={{ x: "10vw", opacity: 0 }}
+          animate={inView ? { x: 0, opacity: 1 } : { x: "10vw", opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          <div className="contentContainer">
+            <h4>Bine ai venit</h4>
+            <h5>Sunt Frontend Developer</h5>
+
+            <div className="contentDescription">
+              <p>
+                > Dezvoltare de site-uri web responsive: creez site-uri web care
+                se adaptează și funcționează perfect pe diferite dispozitive și
+                dimensiuni de ecran, asigurând o experiență optimă pentru
+                utilizator.
+              </p>
+              <br></br>
+              <p>
+                > Dezvoltare React: Construiți aplicații web dinamice și
+                interactive folosind biblioteca React JavaScript, creând
+                componente reutilizabile și implementând managementul stării.
+              </p>
+              <br></br>
+              <p>
+                > Dezvoltare WordPress: creez site-uri web folosind WordPress,
+                permițând clienților să gestioneze și să
+                actualizeze cu ușurință conținutul site-ului web prin interfața
+                WordPress.
+              </p>
+              <br></br>
+              <p>{brand}</p>
+            </div>
+            <div className="infoContainer">
+              <div className="row">
+                <div className="col-12 col-md-6 info">
+                  <span>Nume:</span>
+                  <p>Marc Lupanciuc</p>
+                </div>
+                <div className="col-12 col-md-6 info">
+                  <span>Email:</span>
+                  <p>
+                    <a href={`mailto:${email}`}>{email}</a>
+                  </p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12 col-md-6 info">
+                  <span>Locația: </span>
+                  <p>{location}</p>
+                </div>
+                <div className="col-12 col-md-6 info">
+                  <span>Disponibilitatea:</span>
+                  <p>{availability}</p>
+                </div>
+              </div>
+            </div>
+            <div className="buttonContainer">
+              <button
+                className="btn downloadCV"
+                onClick={handleDownload}
+                disabled={downloading}
+              >
+                {downloading ? "Se descarcă..." : "Descarcă CV"}
+              </button>
+              <SocialIcons />
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
   );
 };
 
