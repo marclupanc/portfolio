aef7c2f68a402f5a09caada64739bc82c0e8d117
Iulian Cirlig <iulian@fuselabcreative.com>
2024-04-15 18:43:20 +0300

    webhook test2

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -38,7 +38,7 @@ const Landing = ({ name }) => {
       paddingBottom: "28px",
     },
   };
-  console.log('webhooktest 2');
+  console.log('webhooktestt 2');
   return (
     <section className="landing" style={styles.landing}>
       <Draw />
