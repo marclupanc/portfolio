332647fd7d811f0625a973e4ee1d8cf4bd447de2
Marc Lupanciuc <marclupanc@gmail.com>
2024-04-15 20:47:59 +0300

    hello

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -38,7 +38,7 @@ const Landing = ({ name }) => {
       paddingBottom: "28px",
     },
   };
-  console.log('webhooktes t ');
+  console.log('webhooktes ');
   return (
     <section className="landing" style={styles.landing}>
       <Draw />
