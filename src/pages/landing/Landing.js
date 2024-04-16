b4e31cdb29719b7b58ee0cbbeb3d7e1db820fdb8
Marc Lupanciuc <marclupanc@gmail.com>
2024-04-16 14:10:39 +0300

    test

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -38,7 +38,7 @@ const Landing = ({ name }) => {
       paddingBottom: "28px",
     },
   };
-  console.log('webhooktessdjt');
+  console.log('webhook tessdjt');
   return (
     <section className="landing" style={styles.landing}>
       <Draw />
